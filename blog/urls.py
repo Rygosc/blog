c3d51fb4e27f5c4e877c7a0f2f89842e9758930a
Rygos <danayazetta@gmail.com>
2024-11-18 14:20:56 +0200

    added the following urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
-    path("", views.post_list, name='post_list')
-    ]
+    path("", views.post_list, name="post_list"),
+    path("post/<int:pk>/", views.post_detail, name="post_detail"),
+    path("post/new/", views.post_new, name="post_new"),
+    path("post/<int:pk>/edit/", views.post_edit, name="post_edit"),
+    path("post/<int:pk>/delete/", views.post_delete, name="post_delete"),
+    path("register/", views.register, name="register"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+        path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
